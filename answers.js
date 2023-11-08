0490c268f09acad03d846efcd0be9b0ca71b5ca6
Shelby Campbell <shelbyacampbell2724@gmail.com>
2023-11-08 17:24:03 -0500

    FizzBuzzSolved

--- answers.js
+++ answers.js
@@ -13,10 +13,21 @@ for (let i = 2; i < 201; i += 2){
 }
 // Since the initial loop is true, then starting the log at 2 where it doesn't increase passed 200 hundred
 // and each number is added to by 2 (i+=2) where the expression (+=) is addition, then this logs every even number b/w 0 and 200
+
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
+for (let i = 1; i < 101; i++) { //provides the scope for what will be logged
+    console.log(i) // logs 1 through 100 in succsession
+if (i / 3) { // determines that if a number is divisible by 3, that Fizz will log instead of the number
+    console.log("Fizz")
+} if (i / 5) { // determines that if a number is divisible by 5, that Buzz will log instead of the number
+    console.log("Buzz")
+} if (i / 3 && 5) // determines that if a number is divisible by both 3 AND 5, that FizzBuzz will log instead of the number
+    console.log("FizzBuzz")
 
+
+}
 
 ////////////////////////////////
 // Wild Wild Life
